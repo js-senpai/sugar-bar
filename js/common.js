51424d434cd0652e3511ca66c60514805a78eb27
karapv <andrewkarapenkov@gmail.com>
2019-01-23 17:22:29 +0200

    Первый этап

--- js/common.js
+++ js/common.js
@@ -1,32 +1,3 @@
 $(function() {
 
-	$(document).ready(function(){
-		$('.slider-main').owlCarousel({
-			items:1,
-			loop:true,
-			nav:false,
-			dots: true,
-			autoplay: true
-		});
-		$('.experts-slider').owlCarousel({
-			items:1,
-			loop:true,
-			nav:true,
-			dots: false,
-			autoplay: true,
-			navText: ["<img src='../../img/arrow.png' alt='arrow'>","<img src='../../img/arrow.png' alt='arrow'>"]
-		});
-		$(window).scroll(function(){
-			if($(this).scrollTop()>=636){
-				$('.header-top').css('display','none');
-			}else{
-				$('.header-top').css('display','flex');
-			}
-		});
-		$('.benefits-number').spincrement({
-			from: 0,
-			duration: 10000
-		})
-	})
-
 });
